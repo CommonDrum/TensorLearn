fa12434af4c2b31eedf94b2a3699ee1558b61a71
CommonDrum <karol.hetman@protonmail.com>
2023-11-09 11:50:02 +0100

    first commit

---
+++ CMakeFiles/TensorLearn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TensorLearn.dir/main.cpp.o"
+  "CMakeFiles/TensorLearn.dir/main.cpp.o.d"
+  "TensorLearn"
+  "TensorLearn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TensorLearn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
